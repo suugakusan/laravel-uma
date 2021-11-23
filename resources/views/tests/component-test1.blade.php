181f3fd1570605d6de75b64849ea673dd17b6964
suugakusan <niniseseko@gmail.com>
2021-11-23 17:12:31 +0900

    属性バック

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
     コンポーネントテスト１
     <x-tests.card title="タイトル" content="本文" :message="$message" />
     <x-tests.card title="タイトル2"  />
+    <x-tests.card title="css" class="bg-red-300"/>
 </x-tests.app>
